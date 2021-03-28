bdd77317c549e99d4c19a531bc2ab013fb52b636
Xinyu Luan <xiluan@ucsd.edu>
2021-03-27 20:32:15 -0700

    edit adminDTO

--- src/main/java/com/hbu/backend/model/dto/AdminDTO.java
+++ src/main/java/com/hbu/backend/model/dto/AdminDTO.java
@@ -1,22 +1,31 @@
 package com.hbu.backend.model.dto;
 
 public class AdminDTO {
-    private Long adminDTO;
-    private String adminName;
+    private Long adminId;
+    private String firstName;
+    private String lastName;
 
-    public Long getAdminDTO() {
-        return adminDTO;
+    public Long getAdminId() {
+        return adminId;
     }
 
-    public void setAdminDTO(Long adminDTO) {
-        this.adminDTO = adminDTO;
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
     }
 
-    public String getAdminName() {
-        return adminName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setAdminName(String adminName) {
-        this.adminName = adminName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
