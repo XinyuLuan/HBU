403dae1d9c642705e2651478588471c7fd7cba34
johnsonFreemanClarke <johnsonjwei@gmail.com>
2021-04-05 11:29:39 -0700

    updated student and instructor

---
+++ src/main/java/com/hbu/backend/model/dto/StudentTranscriptDTO.java
@@ -0,0 +1,15 @@
+package com.hbu.backend.model.dto;
+
+import java.util.List;
+
+public class StudentScriptDTO {
+
+    private StudentDTO student;
+    private List<CourseStudentDTO> couses;
+    private double totalGpa;
+    /*
+    ..... from block board.....
+     */
+
+
+}
