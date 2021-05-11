dd33edf8dc72ffafbabcbf21cef747874d1cbfe4
johnsonFreemanClarke <johnsonjwei@gmail.com>
2021-05-10 19:47:36 -0700

    added GUI components for login and admin services

--- src/main/java/com/hbu/backend/BackendApplication.java
+++ src/main/java/com/hbu/backend/BackendApplication.java
@@ -1,11 +1,25 @@
 package com.hbu.backend;
 
+import com.hbu.backend.repository.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+	@Resource
+	FilesStorageService storageService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
 	}
 }
---
+++ src/main/java/com/hbu/backend/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.hbu.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/static/css/**", "/static/js/**").permitAll()
+                .antMatchers("/adminhome").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(authenticationSuccessHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable(); // we'll enable this in a later blog post
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");
+    }
+}
